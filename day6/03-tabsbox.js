79c89670b313de0cfbe5c766795e936712152980
yilin <yilin@souche.com>
2022-09-16 17:31:30 +0800

    code sync

---
+++ day6/03-tabsbox.js
@@ -0,0 +1,16 @@
+class Tab {
+    constructor(id) {
+        
+    }
+
+    // 1.切换功能
+    toggleTab() {}
+    // 2.添加功能
+    addTab() {}
+    // 3.删除功能
+    removeTab() {}
+    // 4.编辑功能
+    editTab() {}
+}
+
+new Tab('#tab');
